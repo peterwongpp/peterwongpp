5c0b993dbc587a7c3152325c446c57023c514480
Peter Wong <peter@peterwongpp.com>
2014-10-03 01:59:48 +0800

    Backend authorization.

--- app/controllers/backend_controller.rb
+++ app/controllers/backend_controller.rb
@@ -1,3 +1,9 @@
 class BackendController < ApplicationController
   layout "backend"
+
+  before_action :authenticate_admin!
+
+  protected def authenticate_admin!
+    render :unauthorized, layout: "frontend" unless current_user && current_user.admin?
+  end
 end
