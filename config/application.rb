f646602e17507cd93e6001784e8e3abb6db224fe
Peter Wong <peter@peterwongpp.com>
2014-10-05 15:53:53 +0800

    Bump version number to 0.0.1.

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module Peterwongpp
   class Application < Rails::Application
+    config.version = "0.0.1"
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
