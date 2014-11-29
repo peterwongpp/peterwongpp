011caf80ef5f46038579a193a0fc9533d6cdad2c
Peter Wong <peter@peterwongpp.com>
2014-11-30 00:05:53 +0800

    Minor update.

--- app/controllers/backend/posts_controller.rb
+++ app/controllers/backend/posts_controller.rb
@@ -1,6 +1,6 @@
 class Backend::PostsController < BackendController
   def index
-    @posts = Post.all
+    @posts = Post.all.includes(:tags, :categories)
 
     @posts = @posts.page(params[:page])
     @posts = @posts.per(params[:per])
